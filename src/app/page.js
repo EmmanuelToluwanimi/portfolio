d8d7218745b1889c002ba5af83dd00546b4c4109
emmanueltoluwanimi <emmanueltoluwanimi01@gmail.com>
2023-11-27 10:35:21 +0100

    style fix

--- src/app/page.js
+++ src/app/page.js
@@ -8,6 +8,14 @@ export default function Home() {
     window.location.href = params;
   }
 
+  function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+  
+
   return (
     <>
       <nav id="desktop-nav">
@@ -35,29 +43,29 @@ export default function Home() {
       <nav id="hamburger-nav">
         <div className="logo">John Doe</div>
         <div className="hamburger-menu">
-          <div className="hamburger-icon" onClick="toggleMenu()">
+          <div className="hamburger-icon" onClick={toggleMenu}>
             <span />
             <span />
             <span />
           </div>
           <div className="menu-links">
             <li>
-              <a href="#about" onClick="toggleMenu()">
+              <a href="#about" onClick={toggleMenu}>
                 About
               </a>
             </li>
             <li>
-              <a href="#experience" onClick="toggleMenu()">
+              <a href="#experience" onClick={toggleMenu}>
                 Experience
               </a>
             </li>
             <li>
-              <a href="#projects" onClick="toggleMenu()">
+              <a href="#projects" onClick={toggleMenu}>
                 Projects
               </a>
             </li>
             <li>
-              <a href="#contact" onClick="toggleMenu()">
+              <a href="#contact" onClick={toggleMenu}>
                 Contact
               </a>
             </li>
